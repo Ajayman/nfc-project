30862b53777c0060ba7b6b2195c9814430e321f8
System Administrator <root@Macbooks-MacBook-Air.local>
2025-10-06 18:18:21 +0545

    edit category

--- app/api/admin/category/delete/[catId]/route.ts
+++ app/api/admin/category/delete/[catId]/route.ts
@@ -1,19 +1,19 @@
-// import prisma from "@app/lib/prisma";
-// import { NextRequest, NextResponse } from "next/server";
+import prisma from "@app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function DELETE(request: NextRequest, response: NextResponse, {params}:{params: {catId: string}}){
-//     const {catId} = params;
-//     try {
-//         const resultCategory = await prisma.category.delete({
-//             where: {
-//                 id: catId
-//             }
-//         })
-//         if(resultCategory){
-//             return NextResponse.json({message: `deleted Category ${catId}`})
-//         }
-//         return `Cannot Delete Category ${catId} `
-//     }catch(err){
-//         console.error(`Error deleting category `, err)
-//     }
-// }
+export async function DELETE(request: NextRequest, response: NextResponse, {params}:{params: {catId: string}}){
+    const {catId} = params;
+    try {
+        const resultCategory = await prisma.category.delete({
+            where: {
+                id: catId
+            }
+        })
+        if(resultCategory){
+            return NextResponse.json({message: `deleted Category ${catId}`})
+        }
+        return `Cannot Delete Category ${catId} `
+    }catch(err){
+        console.error(`Error deleting category `, err)
+    }
+}
